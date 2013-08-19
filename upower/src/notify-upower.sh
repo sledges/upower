91a43a738099f6febcd5f995fbd273d5dba87a5e
Ruediger Gad <r.c.g@gmx.de>
2013-08-19 12:33:42 +0300

    Fix path to dbus-send in notify-upower.sh
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

--- upower/src/notify-upower.sh
+++ upower/src/notify-upower.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[ "$1" = "post" ] && exec /usr/bin/dbus-send	\
+[ "$1" = "post" ] && exec /bin/dbus-send	\
 	--system --type=signal			\
 	--dest=org.freedesktop.UPower		\
 	/org/freedesktop/UPower			\
